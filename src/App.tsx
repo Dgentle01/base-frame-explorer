fbcea0e9911812f79972bce5dd3be5a8f4c9c353
Seyi Sennuga <oluseyisennuga015@gmail.com>
2025-05-27 21:25:06 -0700

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,34 +8,14 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { WalletProvider } from "@/context/WalletContext";
 import ApiCredentials from "./pages/ApiCredentials";
-<<<<<<< HEAD
 import CreateCoinPage from "./components/CoinManager";
-=======
 import CoinsPage from "./pages/Coins";
 import CoinDetailsPage from "./pages/CoinDetails";
->>>>>>> 2c986637edff5b4dc0c9c782a19498b43d8a8ab6
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-<<<<<<< HEAD
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <WalletProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/api-credentials" element={<ApiCredentials />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-            <Route path="/create-coin" element={<CreateCoinPage />} />
-          </Routes>
-        </BrowserRouter>
-      </WalletProvider>
-    </TooltipProvider>
-=======
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <TooltipProvider>
         <WalletProvider>
@@ -47,6 +25,7 @@ const App = () => (
               <Route path="/coins" element={<CoinsPage />} />
               <Route path="/coins/:address" element={<CoinDetailsPage />} />
               <Route path="/api-credentials" element={<ApiCredentials />} />
+              <Route path="/create-coin" element={<CreateCoinPage />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
             <Toaster />
@@ -55,7 +34,6 @@ const App = () => (
         </WalletProvider>
       </TooltipProvider>
     </ThemeProvider>
->>>>>>> 2c986637edff5b4dc0c9c782a19498b43d8a8ab6
   </QueryClientProvider>
 );
 
